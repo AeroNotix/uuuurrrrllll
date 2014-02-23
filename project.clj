04bd4079b8c012a24b4a5bbd9e18ae2831fea424
Aaron France <aaron.l.france@gmail.com>
2014-02-23 15:12:56 +0100

    Add endpoint to list all short URLs.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot uuuurrrrllll.core
   :dependencies [[compojure "1.1.6"]
+                 [hiccup "1.0.5"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/core.cache "0.6.3"]
                  [ring "1.2.1"]
--- src/uuuurrrrllll/core.clj
+++ src/uuuurrrrllll/core.clj
@@ -3,7 +3,8 @@
             [clojure.core.cache :as cache]
             [ring.middleware.json :refer [wrap-json-body
                                           wrap-json-response]]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty])
+  (:use [hiccup.core]))
 
 
 ;; One day in ms.
@@ -40,9 +41,18 @@
     {:status 301 :headers {"location" (@url-map url)}}
     {:status 404}))
 
+(defn list-all [request]
+  {:status 200
+   :body (html [:body
+                [:ul
+                 (for [[k v] (seq @url-map)]
+                   [:li [:a {:href v} k]])]])})
+
 (defroutes app
   (POST "/" request
         handle-post)
+  (GET "/list/" request
+       list-all)
   (GET "/:url/" request
        handle-get))
 
