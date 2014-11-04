48710b2123409964ecc0b2c2b56a548c5c77d89b
Aaron France <aaron.l.france@gmail.com>
2014-11-05 00:17:02 +0100

    PEP8

--- resources/uuuurrrrllll.py
+++ resources/uuuurrrrllll.py
@@ -13,12 +13,12 @@ URL_SERVER = "http://zerolength.com:8080/"
 URL_SERVER_EXTERNAL = "http://zerolength.com:8080/%s/"
 
 
-def extract_urls(s):
+def extract_urls(msg):
     return re.findall(
-        'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]'
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]'
         '|'
-        '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
-        s
+        r'[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+        msg
     )
 
 
@@ -26,12 +26,12 @@ def shorten_url(data, buf, date, tags, displayed, hilight, prefix, msg):
     channel = weechat.buffer_get_string(buf, "name").split(".")[1]
     shortened = []
     my_nick = weechat.buffer_get_string(buf, 'localvar_nick')
-    if prefix == my_nick:
+    if prefix.lower() == my_nick.lower():
         return weechat.WEECHAT_RC_OK
     for url in extract_urls(msg):
         if len(URL_SERVER_EXTERNAL) + 6 > len(url):
             continue
-        r = requests.post(
+        resp = requests.post(
             URL_SERVER,
             data=json.dumps({
                 'url':     url,
@@ -39,7 +39,7 @@ def shorten_url(data, buf, date, tags, displayed, hilight, prefix, msg):
                 'channel': channel
             }),
             headers={'content-type': 'application/json'})
-        shortened.append(r.json()['short'])
+        shortened.append(resp.json()['short'])
     if shortened:
         weechat.prnt(buf, ' | '.join([URL_SERVER_EXTERNAL
                                       % url for url in shortened]))
